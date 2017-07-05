bc7c80d3aeda050deebf9d1d131d6242b419faca
Haksun Kim <itpro@haksunkim.com>
2017-07-05 20:40:39 +1000

    Add Configuration, Template, Resources

--- app/controller/homeController.go
+++ app/controller/homeController.go
@@ -1,11 +1,16 @@
 package controller
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
+	"fmt"
 )
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(200)
-	fmt.Fprintf(w, "Testing")
+	t, _ := template.ParseGlob("resources/templates/layout/*")
+	t.ParseFiles("resources/templates/home/index.html")
+	err := t.ExecuteTemplate(w, "layout", nil)
+	if err != nil {
+		fmt.Printf(err.Error(), w)
+	}
 }
--- main.go
+++ main.go
@@ -1,19 +1,34 @@
 package main
 
 import (
+	"github.com/gorilla/mux"
+	"github.com/haksunkim/flexiportal/app/controller"
 	"log"
 	"net/http"
-	"github.com/haksunkim/flexiportal/app/controller"
-	"github.com/gorilla/mux"
+	"fmt"
+	"io/ioutil"
+	"encoding/json"
 )
 
+type Configuration struct {
+	AppName string
+	DB struct {
+		Host string
+		User string
+		Password string
+		Port uint
+	}
+}
+
+var Config Configuration
+
 func main() {
+	bac,_ := ioutil.ReadFile("config.json")
+	json.Unmarshal(bac, &Config)
+	
 	r := mux.NewRouter()
 	r.HandleFunc("/", controller.HomeHandler)
+	r.PathPrefix("/resources/").Handler(http.StripPrefix("/resources/", http.FileServer(http.Dir("./resources"))))
 	http.Handle("/", r)
-	srv := &http.Server{
-		Handler: r,
-		Addr:    "127.0.0.1:8000",
-	}
-	log.Fatal(srv.ListenAndServe())
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
